c421ea7d6a0106d44e8555aea12f97e60c6d3987
ruqishang <ruqishang@linkdoc.com>
2020-03-05 21:44:34 +0800

    绘制弹片尾巴，但性能急剧降低，能支持的弹片数量急剧下降

--- firework.js
+++ firework.js
@@ -1,5 +1,5 @@
 (function() {
-    var GRAVITY = 9.8 * 3;
+    var GRAVITY = 9.8 * 6;
 
     var canvas = null;
     var c = null;
@@ -16,7 +16,9 @@
 
         setInterval(function () {
             if (Math.random() < 0.8) {
-                fireworks.push(new Firework());
+                if (fireworks.length < 1) {
+                    fireworks.push(new Firework());
+                }
             }
             console.log(fireworks.length);
         }, 1000);
@@ -49,18 +51,18 @@
         var ly = Math.random() * window.innerHeight / 2;
 
         // 爆炸产生的弹片数量
-        var fragCount = (Math.floor(Math.random() * 20)) + 480;
+        var fragCount = (Math.floor(Math.random() * 10)) + 100;
         for (var i = 0; i < fragCount; i++) {
-            var randStrength = 80 + Math.random() * 3;
+            var randStrength = 200 + Math.random() * 3;
             var randv = randomVelocity(randStrength);  // random velocity
-            var randr = Math.random() * 2 + 2;  // random radius
+            var randr = Math.random() * 1 + 3;  // random radius
             var frag = new Fragment(lx, ly, randv.x, randv.y, randr);
             this.fragmentLst.push(frag);
         }
     }
 
     /**
-     * 生辰随机速度
+     * 生成随机速度
      * @param strength 速度矢量模
      * @returns {x: x方向分量, y: y方向分量}
      * */
@@ -88,6 +90,9 @@
      */
     Firework.prototype.draw = function (timeDiff) {
         this.fragmentLst.forEach(function (frag, i) {
+            frag.drawTail(timeDiff);
+        });
+        this.fragmentLst.forEach(function (frag, i) {
             frag.draw(timeDiff);
         });
         this.fragmentLst = this.fragmentLst.filter(function (frag) {
@@ -95,6 +100,7 @@
         });
     };
 
+    /** 这次爆炸是否已经完全看不见了 */
     Firework.prototype.isGone = function () {
         return this.fragmentLst.length <= 0;
     };
@@ -108,7 +114,9 @@
         this.vx = vx;  // velocity x
         this.vy = vy;  // velocity y
         this.ra = ra;  // fragment radius
-        this.ttl = ttl || (Math.random() * 1 + 3);  // time to life (in seconds)
+        this.ttl = ttl || (Math.random() * 0.5 + 2.2);  // time to life (in seconds)
+        this.tail = [];
+        this.isBurnUp = false;  // 是否已烧完
     }
 
     /**
@@ -116,30 +124,96 @@
      * @param timeDiff 离上一次重绘过了 timeDiff 秒
      */
     Fragment.prototype.draw = function (timeDiff) {
-        this.ttl -= timeDiff;
+        // 只有尚未烧完时，才需重绘
+        if (!this.isBurnUp) {
+            c.save();
+            c.beginPath();
+            c.fillStyle = 'yellow';
+            c.arc(this.lx, this.ly, this.ra, 0, Math.PI * 2);
+            c.fill();
+            c.restore();
+        }
+    };
 
-        this.lx += (this.vx * timeDiff);
-        this.ly += (this.vy * timeDiff);
-        this.vy = this.vy + GRAVITY * timeDiff;
-
-        if (this.ttl <= 2) {
-            this.ra = this.ra * 0.99;
+    Fragment.prototype.drawTail = function (timeDiff) {
+        // 尚未烧完时，迭代状态，并留下尾巴上的一点
+        if (!this.isBurnUp) {
+            this.ttl -= timeDiff;
+    
+            var lx0 = this.lx, ly0 = this.ly;
+            this.lx += (this.vx * timeDiff);
+            this.ly += (this.vy * timeDiff);
+            this.vy = this.vy + GRAVITY * timeDiff;
+            var lx1 = this.lx, ly1 = this.ly;
+    
+            if (this.ttl <= 2) {
+                this.ra = this.ra * 0.9;
+            }
+            if (this.ttl <= 0 || this.ra < 1.5) {
+                this.isBurnUp = true;
+            }
+    
+            // new tail spot
+            this.tail.push(new TailSpot(lx0, ly0, lx1, ly1, this.ra, 255, 255, 0));
         }
 
-        c.save();
-        c.beginPath();
-        c.fillStyle = 'yellow';
-        c.arc(this.lx, this.ly, this.ra, 0, Math.PI * 2);
-        c.fill();
-        // c.fillRect(this.lx, this.ly, this.ra, this.ra);
-        c.restore();
+        // 不论是否烧完，尾巴都需迭代重绘，并把已经消散的尾巴片段去掉
+        this.tail.forEach(function (ts, i) {
+            ts.draw(timeDiff);
+        });
+        this.tail = this.tail.filter(function (ts) {
+            return !ts.isGone();
+        });
+    };
+
+    /** 这片礼花弹是否已经看不见了 */
+    Fragment.prototype.isGone = function () {
+        // 当自己本身已经烧完，尾巴全部消散，则完全看不见了
+        return this.isBurnUp && this.tail.length <= 0;
     };
 
     /**
-     * 这片礼花弹是否已经看不见了
-     */
-    Fragment.prototype.isGone = function () {
-        return (this.ttl <= 0) || (this.ra <= 3);
+     * 弹片尾巴上的光点
+     * 
+     * 每个光点实际上是弹片轨迹中间的一小段直线，因为采样率非常高，看起来非常平滑
+     * 每一小段的亮度不同，导致看起来整条轨迹弧线的亮度是均匀变化的，制造出尾巴逐渐消散的效果
+     * */
+    function TailSpot(lx0, ly0, lx1, ly1, ra, r, g, b) {
+        this.lx0 = lx0;  // 起始位置 x 分量
+        this.ly0 = ly0;  // 起始位置 y 分量
+        this.lx1 = lx1;  // 终止位置 x 分量
+        this.ly1 = ly1;  // 终止位置 y 分量
+        this.ra = ra;    // 线段宽度
+        this.light = 1;  // 亮度，1 为全亮，0 为全消散
+        this.r = r;  // 当前颜色 r 分量
+        this.g = g;  // 当前颜色 g 分量
+        this.b = b;  // 当前颜色 b 分量
+    }
+
+    TailSpot.prototype.draw = function (timeDiff) {
+        c.save();
+        c.strokeStyle = rgb(this.r * this.light, this.g * this.light, this.b * this.light);
+        c.lineWidth = this.ra;
+        c.lineCap = 'round';
+        c.beginPath();
+        c.moveTo(this.lx0, this.ly0);
+        c.lineTo(this.lx1, this.ly1);
+        c.stroke();
+        c.restore();
+
+        this.ra = this.ra * 0.96;
+        this.light = this.light * 0.94;
+        
     };
+
+    /** 尾巴上的这一小片段是否已经完全消散 */
+    TailSpot.prototype.isGone = function () {
+        // 亮度非常低，或者宽度非常小时，几乎看不见
+        return this.light < 0.2 || this.ra < 1;
+    }
+
+    function rgb(r, g, b) {
+        return 'rgb(' + r + ',' + g + ',' + b + ')';
+    }
 
 })();
